0e156c22d120cfb4d8154d87cebbce2b6bd8ab51
VIVunt <dashenko.ilya2014@yandex.ru>
2021-04-05 17:46:28 +0300

    [Laba3]

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,14 @@
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -39,9 +47,9 @@
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
-            "Отрезок",
-            "Прямоугольник",
-            "Эллипс",
+            "Laba1.DrawLine",
+            "Laba1.DrawRectangle",
+            "Laba1.DrawEllipse",
             "Ломаная",
             "Многоугольник"});
             this.comboBox1.Location = new System.Drawing.Point(0, 0);
@@ -92,11 +100,84 @@
             this.comboBox4.Size = new System.Drawing.Size(121, 21);
             this.comboBox4.TabIndex = 3;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "X1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Y1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "X2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Y2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 87);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 16;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(49, 116);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 17;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(49, 142);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 18;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(49, 168);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 19;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(936, 449);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox4);
             this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.comboBox2);
@@ -104,8 +185,11 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -115,6 +199,14 @@
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.ComboBox comboBox3;
         private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
 
 
 
--- Form1.cs
+++ Form1.cs
@@ -6,16 +6,22 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Reflection;
+using System.Drawing.Drawing2D;
 
 namespace Laba1
 {
-    public interface IDraw
-    {
-        void Draw();
-    }
-
     public partial class Form1 : Form
     {
+        public delegate void DrawDelegate();
+        public event DrawDelegate ToDraw;
+        private IDraw figure;
+        private bool DrawPermission = false;
+        private Graphics Graph;
+        private int X1;
+        private int Y1;
+        private Assembly assembly = Assembly.GetExecutingAssembly();
+
         public Form1()
         {
             InitializeComponent();
@@ -23,15 +29,189 @@ namespace Laba1
             comboBox2.SelectedIndex = 0;
             comboBox3.SelectedIndex = 0;
             comboBox4.SelectedIndex = 0;
+            Graph = this.CreateGraphics();
         }
 
-        private void Form1_Paint(object sender, PaintEventArgs e)
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
-            Graphics graph = this.CreateGraphics();
-            Pen pen = new Pen(Color.Red);
+            this.X1 = Form1.MousePosition.X;
+            this.Y1 = Form1.MousePosition.Y;
 
-            graph.DrawLine(pen, 50, 90, 170, 190);
-            graph.Dispose();
+            Type type = assembly.GetType(comboBox1.SelectedItem.ToString());
+            figure = Activator.CreateInstance(type) as IDraw;
+            figure.Init(X1, Y1, 0, 0, 0, this);
+
+            DrawPermission = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Form1.MouseButtons == MouseButtons.Left && DrawPermission)
+            {
+                figure.X2 = Form1.MousePosition.X - 1;
+                figure.Y2 = Form1.MousePosition.Y - 23;
+
+                Graph.Clear(System.Drawing.SystemColors.Control);
+                if (ToDraw != null) ToDraw();
+                figure.Draw();
+
+                textBox1.Text = figure.X1.ToString();
+                textBox2.Text = figure.Y1.ToString();
+                textBox3.Text = figure.X2.ToString();
+                textBox4.Text = figure.Y2.ToString();
+
+            } else DrawPermission = true;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if ((figure.Width != 0) || (figure.Height != 0))
+            {
+                ToDraw += figure.Draw;
+            }
+            figure = null;
+            DrawPermission = false;
         }
     }
+
+    public interface IDraw
+    {
+        void Draw();
+        void Init(int x1, int y1, int x2, int y2, int thickness, Form1 form);
+        int X1 { get; set; }
+        int Y1 { get; set; }
+        int X2 { get; set; }
+        int Y2 { get; set; }
+        int Width { get; set; }
+        int Height { get; set; }
+    }
+
+    public class DrawRectangle: Object, IDraw
+    {
+        public int X1 { get; set; }
+        public int Y1 { get; set; }
+        public int X2 { get; set; }
+        public int Y2 { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Thickness { get; set; }
+        public Form1 form { get; set; }
+        public int FrontColor { get; set; }
+
+        public void Init(int x1, int y1, int x2, int y2, int thickness, Form1 form)
+        {
+            this.X1 = x1;
+            this.Y1 = y1;
+            this.X2 = x2;
+            this.Y2 = y2;
+            this.Thickness = thickness;
+            this.form = form;
+        }
+
+        public void Draw()
+        {
+            Width = Math.Abs(X2 - X1) + 1;
+            Height = Math.Abs(Y2 - Y1) + 1;
+
+            int start1, start2;
+
+            if (X1 <= X2) start1 = X1;
+            else start1 = X2;
+
+            if (Y1 <= Y2) start2 = Y1;
+            else start2 = Y2;
+
+            SolidBrush brush = new SolidBrush(Color.Red);
+            Graphics graph = form.CreateGraphics();
+            Pen pen = new Pen(Color.Blue, 10);
+            pen.Alignment = PenAlignment.Inset;
+            graph.DrawRectangle(pen, start1, start2, Width, Height);
+            graph.FillRectangle(brush, start1 + 10, start2 + 10, Width - 20, Height - 20);
+            graph.Dispose();
+            pen.Dispose();
+            brush.Dispose();
+        }
+    }
+
+    public class DrawEllipse : Object, IDraw
+    {
+        public int X1 { get; set; }
+        public int Y1 { get; set; }
+        public int X2 { get; set; }
+        public int Y2 { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Thickness { get; set; }
+        public Form1 form { get; set; }
+        public int FrontColor { get; set; }
+
+        public void Init(int x1, int y1, int x2, int y2, int thickness, Form1 form)
+        {
+            this.X1 = x1;
+            this.Y1 = y1;
+            this.X2 = x2;
+            this.Y2 = y2;
+            this.Thickness = thickness;
+            this.form = form;
+        }
+
+        public void Draw()
+        {
+            Width = Math.Abs(X2 - X1) + 1;
+            Height = Math.Abs(Y2 - Y1) + 1;
+
+            int start1, start2;
+
+            if (X1 <= X2) start1 = X1;
+            else start1 = X2;
+
+            if (Y1 <= Y2) start2 = Y1;
+            else start2 = Y2;
+
+            SolidBrush brush = new SolidBrush(Color.Red);
+            Graphics graph = form.CreateGraphics();
+            Pen pen = new Pen(Color.Blue, 10);
+            pen.Alignment = PenAlignment.Inset;
+            graph.DrawEllipse(pen, start1, start2, Width, Height);
+            graph.FillEllipse(brush, start1 + 10, start2 + 10, Width - 20, Height - 20);
+            graph.Dispose();
+            pen.Dispose();
+            brush.Dispose();
+        }
+    }
+
+    public class DrawLine : Object, IDraw
+    {
+        public int X1 { get; set; }
+        public int Y1 { get; set; }
+        public int X2 { get; set; }
+        public int Y2 { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Thickness { get; set; }
+        public Form1 form { get; set; }
+        public int FrontColor { get; set; }
+
+        public void Init(int x1, int y1, int x2, int y2, int thickness, Form1 form)
+        {
+            this.X1 = x1;
+            this.Y1 = y1;
+            this.X2 = x2;
+            this.Y2 = y2;
+            this.Thickness = thickness;
+            this.form = form;
+        }
+
+        public void Draw()
+        {
+            Width = Math.Abs(X2 - X1) + 1;
+            Height = Math.Abs(Y2 - Y1) + 1;
+
+            Graphics graph = form.CreateGraphics();
+            Pen pen = new Pen(Color.Blue, 10);
+            graph.DrawLine(pen, X1, Y1, X2, Y2);
+            graph.Dispose();
+            pen.Dispose();
+        }
+    }  
 }
